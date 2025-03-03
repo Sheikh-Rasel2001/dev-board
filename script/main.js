1243ba2b4bc4da8278f0343fa044e74324c7145d
Sheikh-Rasel2001 <mhrasel191132@gmail.com>
2025-03-03 11:21:36 +0600

    date and time add

--- script/main.js
+++ script/main.js
@@ -6,3 +6,23 @@ button.addEventListener('click', function(){
     const colorIndex = parseInt(Math.random()*colors.length);
     body.style.backgroundColor = colors[colorIndex];
 })
+
+// date object
+const date = new Date();
+    document.getElementById('date').innerText = date.toDateString();
+    // document.getElementById('date').innerText = date.toLocaleTimeString()
+
+document.getElementById('completed-btn').addEventListener('click', function(){
+    const fixMobile = document.getElementById('fix-mobile').innerText;
+    console.log(fixMobile);
+
+    const time = new Date();
+    document.getElementById('active-container').innerText = time.toLocaleTimeString();
+
+    const container = document.getElementById('active-container');
+    const p = document.createElement('p');
+    p.innerText = `
+    You have completed The ${fixMobile} at ${time} 
+    `
+    container.appendChild(p);
+})
